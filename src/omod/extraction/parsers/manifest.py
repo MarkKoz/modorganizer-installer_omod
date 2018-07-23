fee61fed1d577c0be0761e5a4adc23a76a6b374c
MarkKoz <KozlovMark@gmail.com>
2018-07-23 02:27:18 -0700

    Cleaned up parser files.
    * Removed unused imports.
    * Removed version check.
    * Minor code style formatting.
    * Simplified LEB128 value calculation.

--- src/omod/extraction/parsers/manifest.py
+++ src/omod/extraction/parsers/manifest.py
@@ -1,10 +1,6 @@
-from pkg_resources import parse_version
-from kaitaistruct import __version__ as ks_version, KaitaiStruct
+from kaitaistruct import KaitaiStruct
 
-from parsers.vlq_base128_le import VlqBase128Le
-
-if parse_version(ks_version) < parse_version('0.7'):
-    raise Exception("Incompatible Kaitai Struct Python API: 0.7 or later is required, but you have %s" % ks_version)
+from .vlq_base128_le import VlqBase128Le
 
 class Manifest(KaitaiStruct):
     def __init__(self, _io, _parent=None, _root=None):
@@ -16,6 +12,7 @@ class Manifest(KaitaiStruct):
     def _read(self):
         self.files = []
         i = 0
+
         while not self._io.is_eof():
             self.files.append(self._root.File(self._io, self, self._root))
             i += 1
@@ -41,4 +38,5 @@ class Manifest(KaitaiStruct):
 
         def _read(self):
             self.length = VlqBase128Le(self._io)
-            self.name = (self._io.read_bytes(self.length.value)).decode(u"UTF-8")
+            self.name = (self._io.read_bytes(self.length.value)).decode(
+                u"UTF-8")
